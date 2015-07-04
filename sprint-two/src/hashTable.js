34bd37d5e6d04dcb4546c103907001c0524b5ce2
ntoscano <Nicolas.Toscano@gmail.com>
2015-07-04 14:22:38 -0700

    completed tests, stil have to implement re-hashing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,20 +5,33 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var storedVal = this._storage.get(i)
   var value = [];
-  value.push(k, v);
+  if(storedVal){
+    value.push(storedVal, [k, v])
+  }else{
+    value.push(k, v);
+  }
   this._storage.set(i, value);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var arr = this._storage.get(i);
+  if(Array.isArray(arr[0])){
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i][0] === k){
+        return arr[i][1]
+      }
+    }
+  }
   return arr[1];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.get(i) = null;
+  var arr = this._storage.get(i);
+  arr[1] = null;
 };
 
 
